40dc86c6ecc4db30d6882a68d961b9d2a1282c17
Dani Orlando <daniorlando8@gmail.com>
2021-10-30 15:48:17 +0700

    30/10/2021

---
+++ 05 - Statment If/src/Main.java
@@ -0,0 +1,18 @@
+/* ~ Statement IF ~ 
+
+    Statement if adalah jenis control flow yang paling umum. 
+    Dengan meletakkan kondisi didalam () setelah if, code di dalam {} hanya dijalankan saat kondisinya true.
+    Ini penting!
+
+    Mari kita lihat contoh statement if. 
+    Boolean, operator perbandingan (==, <, dll) dan operator logika (&&, ||, !) 
+    Akan sering digunakan dalam kondisi statement if.
+*/
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x == 10){
+            System.out.println("Nilai x = true, x sama dengan 10");
+        }
+    }
+}
---
+++ 05 - Statment If/src/Soal.java
@@ -0,0 +1,30 @@
+/* ~ Soal ~
+ 
+    1. Kerjakanlah soal dibawah ini.
+ */
+public class Soal {
+    public static void main(String[] args) {
+
+        // Berikan true sebagai kondisi untuk statement if.
+        if (){
+            System.out.println("Kondisi adalah true, Cetak...");
+        }
+
+        // Berikan false sebagai kondisi untuk statement if.
+        if (){
+            System.out.println("Kondisi adalah false, Cetak...");
+        }
+
+        // Berikan x sebagai variabel yang bernilai 5.
+        int x = ;
+        // Jika x lebih besar dari 2, cetak "x lebih besar dari 2"
+        if (){
+            System.out.println("x lebih besar dari 2");
+        }
+
+        // Jika x lebih besar dari 8, cetak "x lebih besar dari 9"
+        if (){
+            System.out.println("x lebih besar dari 8");
+        }
+    }
+}
