3d2206564bda6a2aa0073787708c92c60adf5d5f
Dani Orlando <daniorlando8@gmail.com>
2021-10-30 16:23:47 +0700

    30/10/2021

--- 05 - else and else if/src/Main.java
+++ 05 - else and else if/src/Main.java
@@ -1,10 +1,19 @@
+/* ~ else and else if ~
+    
+    Dengan menggunakan statement else, Anda dapat menambahkan beberapa code yang ingin Anda jalankan ketika kondisi statement if adalah false. 
+    Pikirkan else seperti permainan ketika Anda tidak memiliki gerakan lagi. 
+    Lihat contoh di bawah ini untuk mempelajari sintaksnya. 
+    Pastikan untuk memperhatikan posisi kurung kurawal { }.
+*/
 public class Main {
     public static void main(String[] args) {
        int x = 15;
-       if(x > 10){
-           System.out.println("x Lebih besar dari 10");
+       if(x <= 10){
+            System.out.println("x Lebih kecil dari 10"); //false
+       }else if (x > 10 ){
+            System.out.println("x Lebih besar dari 10"); //true
        }else{
-           System.out.println("x Lebih kecil dari 10");
+            System.out.println("x Lebih kecil atau sama dengan 10"); //false
        }
     }
 }
