14b432099b87d0193a5b98944f4e374edd8e7844
Dani Orlando <daniorlando8@gmail.com>
2021-10-30 16:23:35 +0700

    30/10/2021

---
+++ 05 - else and else if/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int x = 15;
+       if(x > 10){
+           System.out.println("x Lebih besar dari 10");
+       }else{
+           System.out.println("x Lebih kecil dari 10");
+       }
+    }
+}
